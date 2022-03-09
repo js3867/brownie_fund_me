b4c28aba06a7009e2595eed4489e21588b75d1dd
js3867 <83634235+js3867@users.noreply.github.com>
2022-03-09 12:44:26 +0800

    pre-forking

--- scripts/fund_and_withdraw.py
+++ scripts/fund_and_withdraw.py
@@ -1,4 +1,4 @@
-from brownie import FundMe
+from brownie import FundMe, network
 from scripts.helpful_scripts import get_account
 
 
@@ -6,11 +6,17 @@ def fund():
     fund_me = FundMe[-1]
     account = get_account()
     entrance_fee = fund_me.getEntranceFee()
-    print(entrance_fee)
-    print(f"The current entrance fee is {entrance_fee}")
-    print("Funding..")
+    print(f"The current entry fee is {entrance_fee}")
+    print("Funding")
     fund_me.fund({"from": account, "value": entrance_fee})
+
+
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
 
 
 def main():
     fund()
+    withdraw()
---
+++ tests/test_fund_me.py
@@ -0,0 +1,30 @@
+from scripts.helpful_scripts import get_account, LOCAL_BLOCKCHAIN_ENVIRONMENTS
+from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
+
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+    fund_me = deploy_fund_me()
+    # by setting exceptions for expected errors (that you are testing for)
+    # this will cause "deselected" result for this outcome
+    bad_actor = accounts.add()
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
+
+
+# brownie test -k test_only_owner_can_withdraw --network rinkeby
